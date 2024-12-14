8d1d86ef99d698a691ff67483d5c4f47f64546b9
elparko <parker.smith.00@icloud.com>
2024-12-14 14:19:33 -0600

    fixed

--- medlineplusfrontend/vite.config.ts
+++ medlineplusfrontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     assetsDir: 'assets',
     sourcemap: true,
     emptyOutDir: true,
